9526811951cbf0e44042e1fc321ff691f8d0984a
Margarida <margarida.martins@ua.pt>
2022-05-26 00:38:07 +0100

    celery long and short

--- docker/celeryworkershortgic3/entrypoint.sh
+++ docker/celeryworkershortgic3/entrypoint.sh
@@ -16,7 +16,7 @@ mkdir -p media_files/hls
 echo "Enabling celery-short task worker"
 echo "Enabling celery-long task worker"
 
-celery worker -A cms --pidfile=/home/mediacms.io/mediacms/pids/%h-%I.pid --logfile=/home/mediacms.io/mediacms/logs/%h-%I.log --loglevel=DEBUG --soft-time-limit=300 -c5 --workdir=/home/mediacms.io/mediacms -Q short_tasks -b 'redis://:a-very-complex-password-here@redis:6379/1'
+celery worker -A cms --pidfile=/home/mediacms.io/mediacms/pids/%h-%I.pid --logfile=/home/mediacms.io/mediacms/logs/%h-%I.log --loglevel=DEBUG --soft-time-limit=300 -c1 --workdir=/home/mediacms.io/mediacms -Q short_tasks -b 'redis://:a-very-complex-password-here@redis:6379/1'
 
 
 

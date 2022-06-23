c339d37c85007724b47b37d3ecf53ddaa62592c7
leand12 <leandrosilva12@ua.pt>
2022-06-23 22:45:34 +0100

    rm secrets.sh

--- k8s/secret.sh
+++
@@ -1,3 +0,0 @@
-kubectl create secret generic prod-db-secret --from-literal=username=mediacms --from-literal=password=$(head -c 24 /dev/random | base64) -n gic3
-kubectl create secret generic prod-redis-secret --from-literal=password=$(head -c 24 /dev/random | base64) -n gic3
-

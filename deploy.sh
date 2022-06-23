58849b347d96918955f3059d901944d5aa744594
leand12 <leandrosilva12@ua.pt>
2022-06-23 22:44:18 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ MEDIACMS_PATH='/home/mediacms.io/mediacms'
 ### secrets ###
 
 REDIS_SECRET=$(head -c 24 /dev/random | base64)
-kubectl create secret generic prod-db-secret --from-literal=username=mediacms --from-literal=password=$(head -c 24 /dev/random | base64) -n gic3
-kubectl create secret generic prod-redis-secret --from-literal=password=$REDIS_SECRET -n gic3
+kubectl create secret generic prod-db-secret --from-literal=username=mediacms --from-literal=password=$(head -c 24 /dev/random | base64) -n $NAMESPACE
+kubectl create secret generic prod-redis-secret --from-literal=password=$REDIS_SECRET -n $NAMESPACE
 
 
 ### config maps ###
